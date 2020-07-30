068b4b9e018e54421183f54a4daaa4a4691cd819
Kirill Krupina <kirill.krupina26@gmail.com>
2020-07-30 14:37:51 +0300

    fetch data from DB. Push to client side

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="col-6 navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Main <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active offset-3">
+                <a class="nav-link" href="/">Create a post<span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        @foreach($posts as $post)
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">{{$post->short_title}}</div>
+                    <div class="card-body">{{$post->descr}}</div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+</body>
+</html>
